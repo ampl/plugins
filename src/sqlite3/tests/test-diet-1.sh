410f7d331d3f97ba2beab7ad1a58e08c9b255332
Filipe Brandao <fdabrandao@gmail.com>
2019-08-01 22:18:40 +0100

    Rename sqlite3.dll driver to sqlite3th.dll
    In order to avoid conflicts with the real sqlite3.dll on Windows

--- src/sqlite3/tests/test-diet-1.sh
+++ src/sqlite3/tests/test-diet-1.sh
@@ -2,6 +2,14 @@
 set -e
 set -x
 cd `dirname $0`
+
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 ampl dll"
+    exit 1
+fi
+AMPL=$1
+TABLE_HANDLER=$2
+
 [ ! -e diet.dat.db ] && (sqlite3 diet.dat.db < diet.dat.sql)
-ampl -i ../../sqlite3.dll diet-sql-1.run > output-1.txt
+$AMPL -i $TABLE_HANDLER diet-sql-1.run > output-1.txt
 diff output-1.txt output-1-expected.txt
--- src/sqlite3/tests/test-diet-2.sh
+++ src/sqlite3/tests/test-diet-2.sh
@@ -2,6 +2,14 @@
 set -e
 set -x
 cd `dirname $0`
+
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 ampl dll"
+    exit 1
+fi
+AMPL=$1
+TABLE_HANDLER=$2
+
 [ ! -e diet.dat.db ] && (sqlite3 diet.dat.db < diet.dat.sql)
-ampl -i ../../sqlite3.dll diet-sql-2.run > output-2.txt
+$AMPL -i $TABLE_HANDLER diet-sql-2.run > output-2.txt
 diff output-2.txt output-2-expected.txt
