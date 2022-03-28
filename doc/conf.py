fee74a20ee0e1219e2b65604d749bca0780796d4
Filipe Brandão <fdabrandao@gmail.com>
2022-03-28 12:09:23 +0100

    Switch to ampl-sphinx-theme

--- doc/conf.py
+++ doc/conf.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
 import sys
-import os
 import subprocess
+import datetime
 
 # Important: keep this as a string or sphinx will fail with
 # a non-helpful error message
@@ -11,36 +11,36 @@ TABLES_VERSION = "20211110"
 # -- General configuration -----------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
-needs_sphinx = '3.2.0'
+needs_sphinx = "3.2.0"
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['breathe', 'sphinx.ext.mathjax', 'sphinx.ext.graphviz']
+extensions = ["breathe", "sphinx.ext.mathjax", "sphinx.ext.graphviz"]
 
 # Configure Breathe.
 # When building with CMake, the path to doxyxml is passed via the command line.
-breathe_projects = {'tables': 'doxyxml'}
-breathe_default_project = 'tables'
-breathe_domain_by_extension = {'h': 'cpp', 'hpp' : 'cpp'}
+breathe_projects = {"tables": "doxyxml"}
+breathe_default_project = "tables"
+breathe_domain_by_extension = {"h": "cpp", "hpp": "cpp"}
 
-highlight_language = 'c++'
-primary_domain = 'cpp'
+highlight_language = "c++"
+primary_domain = "cpp"
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # The suffix of source filenames.
-source_suffix = '.rst'
+source_suffix = ".rst"
 
 # The encoding of source files.
-#source_encoding = 'utf-8-sig'
+# source_encoding = 'utf-8-sig'
 
 # The master toctree document.
-master_doc = 'index'
+master_doc = "index"
 
 # General information about the project.
-project = u'ampl::tables'
-copyright = u'2015-2021 AMPL Optimization, Inc'
+project = "ampl::tables"
+copyright = "2015-{}, AMPL Optimization Inc".format(datetime.date.today().year)
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -54,35 +54,35 @@ release = TABLES_VERSION
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build', 'virtualenv']
+exclude_patterns = ["_build", "virtualenv"]
 
 # The reST default role (used for this markup: `text`) to use for all documents.
-default_role = 'cpp:any'
+default_role = "cpp:any"
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = "sphinx"
 
 # A list of ignored prefixes for module index sorting.
-#modindex_common_prefix = []
+# modindex_common_prefix = []
 
 
 # -- Options for HTML output ---------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'pydata_sphinx_theme'
+html_theme = "ampl_sphinx_theme"
 html_theme_options = {
     "logo_link": "index",
     "icon_links": [
         {
             "name": "GitHub",
             "url": "https://github.com/ampl/plugins",
-            "icon": "fab fa-github"
+            "icon": "fab fa-github",
         },
         {
-            "name": "AMPL Projects",
-            "url": "https://ampl.github.io",
-            "icon": "fas fa-project-diagram fa-fw",
+            "name": "AMPL Resources",
+            "url": "https://developers.ampl.com",
+            "icon": "fas fa-book fa-fw",
             "target": "_self",
         },
         {
@@ -101,16 +101,14 @@ html_theme_options = {
     "collapse_navigation": True,
     "external_links": [
         # {"name": "Try AMPL", "url": "https://ampl.com"}
-    ]
-}
-html_context = {
-    "theme_logo_text": "Plugins",
+    ],
+    "logo_text": "Plugins",
 }
 
-html_static_path = ['_static']
-html_css_files = [
-    'css/custom.css',
-]
+html_static_path = ["_static"]
+# html_css_files = [
+#     'css/custom.css',
+# ]
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
 html_title = "ampl::plugins {} documentation".format(version)
@@ -133,7 +131,7 @@ html_show_sphinx = False
 
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'AMPLdoc'
+htmlhelp_basename = "AMPLdoc"
 
 
 # -- Options for LaTeX output --------------------------------------------------
@@ -141,10 +139,8 @@ htmlhelp_basename = 'AMPLdoc'
 latex_elements = {
     # The paper size ('letterpaper' or 'a4paper').
     # 'papersize': 'letterpaper',
-
     # The font size ('10pt', '11pt' or '12pt').
     # 'pointsize': '10pt',
-
     # Additional stuff for the LaTeX preamble.
     # 'preamble': '',
 }
@@ -152,42 +148,38 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-    ('index', 'tables.tex', u'ampl::plugins Documentation',
-     u'Nicolau Santos', 'manual'),
+    ("index", "tables.tex", "ampl::plugins Documentation", "Nicolau Santos", "manual"),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
-#latex_logo = None
+# latex_logo = None
 
 # For "manual" documents, if this is true, then toplevel headings are parts,
 # not chapters.
-#latex_use_parts = False
+# latex_use_parts = False
 
 # If true, show page references after internal links.
-#latex_show_pagerefs = False
+# latex_show_pagerefs = False
 
 # If true, show URL addresses after external links.
-#latex_show_urls = False
+# latex_show_urls = False
 
 # Documents to append as an appendix to all manuals.
-#latex_appendices = []
+# latex_appendices = []
 
 # If false, no module index is generated.
-#latex_domain_indices = True
+# latex_domain_indices = True
 
 
 # -- Options for manual page output --------------------------------------------
 
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
-man_pages = [
-    ('index', 'ampl', u'AMPL Documentation',
-     [u'Nicolau Santos'], 1)
-]
+man_pages = [("index", "ampl", "AMPL Documentation", ["Nicolau Santos"], 1)]
 
 # If true, show URL addresses after external links.
-#man_show_urls = False
+# man_show_urls = False
 
 
 # -- Options for Texinfo output ------------------------------------------------
@@ -196,19 +188,25 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    ('index', 'AMPL', u'AMPL Documentation',
-     u'Nicolau Santos', 'AMPL', 'One line description of project.',
-     'Miscellaneous'),
+    (
+        "index",
+        "AMPL",
+        "AMPL Documentation",
+        "Nicolau Santos",
+        "AMPL",
+        "One line description of project.",
+        "Miscellaneous",
+    ),
 ]
 
 # Documents to append as an appendix to all manuals.
-#texinfo_appendices = []
+# texinfo_appendices = []
 
 # If false, no module index is generated.
-#texinfo_domain_indices = True
+# texinfo_domain_indices = True
 
 # How to display URL addresses: 'footnote', 'no', or 'inline'.
-#texinfo_show_urls = 'footnote'
+# texinfo_show_urls = 'footnote'
 
 
 def run_doxygen(folder):
@@ -225,7 +223,7 @@ def run_doxygen(folder):
 
 def generate_doxygen_xml(app):
     """Run the doxygen make commands if we're on the ReadTheDocs server"""
-    #read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
+    # read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
     # We always build doxygen documentation
     run_doxygen("./")
 
