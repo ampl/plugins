e13da385b8c2442649d731d91b0b8f018e228377
Your Name <nfbvs@ampl.com>
2022-08-30 21:15:47 -0400

    amplcsv - update version to 0.0.7

--- src/amplp/examples/amplcsv/src/handler.hpp
+++ src/amplp/examples/amplcsv/src/handler.hpp
@@ -7,7 +7,7 @@
 using namespace amplp;
 
 static std::string name = "amplcsv";
-static std::string version = "beta 0.0.6";
+static std::string version = "beta 0.0.7";
 
 class Handler:
 public TableConnector{
