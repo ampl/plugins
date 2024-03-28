0ffd67ec4d937e2dd41f2ba14854d5276f6079f5
Nicolau Santos <nfbvs@ampl.com>
2024-03-28 16:33:36 -0400

    amplsqlite3: add handler description

--- src/amplp/examples/amplsqlite3/src/handler.hpp
+++ src/amplp/examples/amplsqlite3/src/handler.hpp
@@ -11,13 +11,8 @@
 
 using namespace amplp;
 
-
 static std::string name = "amplsqlite3";
 static std::string version = "0.0.0";
-
-static std::string doc = name + "\n" + name + "-" + version + "\n" +
-"amplsqlite3: experimental sqlite3 driver for AMPL.\n"
-;
 
 class Handler:
 public TableConnector{
@@ -75,3 +70,65 @@ public TableConnector{
 
 	~Handler();
 };
+
+static std::string doc = name + "\n" +
+name + " - " + version + "\n" +
+"sqlite3 - " + sqlite3_version + "\n" +
+"A table handler for sqlite3 databases.\n"
+"\n"
+"General information on table handlers and data correspondence between AMPL and \n"
+"an external table is available at chapter 10 of the AMPL book:\n"
+"\n"
+"    https://ampl.com/learn/ampl-book/\n"
+"\n"
+"Information on sqlite3 is available at\n"
+"\n"
+"    https://www.sqlite.org\n"
+"\n"
+"The available options for amplsqlite3 are:\n"
+"\n"
+"alias:\n"
+"    Instead of writing the data to the table with the name defined in the table\n"
+"    declaration it's possible define the table name with an alias. In the\n"
+"    following example the table handler will search for a table named \"bar\"\n"
+"    instead of a table named \"foo\" as in the table declaration.\n"
+"\n"
+"    Example:\n"
+"        table foo OUT \"amplsqlite3\" \"bar\": [A], B;\n"
+"\n"
+"external-table-spec:\n"
+"    Specifies the path to the .db file to be read or written with the read \n"
+"    table and write table commands. If no file is specified, amplsqlite3 will\n"
+"    search for a file with the table name and the .db file extension in the\n"
+"    current directory. If the table is to be written and the file does not exist\n"
+"    it will be created.\n"
+"\n"
+"    Example:\n"
+"        table foo OUT \"amplsqlite3\" \"bar.db\": [keycol], valcol;\n"
+"\n"
+"verbose:\n"
+"    Display warnings during the execution of the read table and write table\n"
+"    commands.\n"
+"\n"
+"    Example:\n"
+"        table foo OUT \"amplsqlite3\" \"verbose\": [keycol], valcol;\n"
+"\n"
+"verbose=option:\n"
+"    Display information according to the specified option. Available options:\n"
+"        0 (default) - display information only on error,\n"
+"        1 - display warnings,\n"
+"        2 - display general information\n"
+"        3 - display debug information.\n"
+"\n"
+"    Example:\n"
+"        table foo OUT \"amplsqlite3\" \"verbose=2\": [keycol], valcol;\n"
+"\n"
+"write=option\n"
+"    Define how the data is written in OUT mode. Available options:\n"
+"        delete (default) - deletes all the rows of the corresponding table (if\n"
+"        it exists) before writing the data from AMPL.\n"
+"        drop - drops the corresponding table and creates a new one.\n"
+"\n"
+"    Example:\n"
+"        table foo OUT \"amplsqlite3\" \"write=drop\": [keycol], valcol;\n"
+;
